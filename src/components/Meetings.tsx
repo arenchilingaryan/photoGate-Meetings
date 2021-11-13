6a63464a26708398a440eecb92de54a4b3acbf69
arenchilingaryan <arenchilingaryan@yandex.ru>
2021-11-13 06:52:39 -0800

    merge

---
+++ src/components/Meetings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const Events = () => {
+  return (
+    <div className="div" style={{fontSize: '100px'}}>
+      Events
+    </div>
+  );
+};
+
+export default Events;
